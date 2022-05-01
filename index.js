d2e3f252a98d6e0daa293a9d18d4d14120959077
Jason <jdmagic21@gmail.com>
2022-05-01 19:57:40 -0400

    fix set height by event handler type

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-var projects_offset_top = 0; 
-var projects_div_height = 0; 
-var projects_offset_bottom = 0; 
-var scroll_from_top = 0; 
-var window_size = 0;
+var projects_offset_top, projects_div_height, 
+projects_offset_bottom, scroll_from_top, 
+window_size, window_height;
+
+setScrollHeight(); 
+setLengthsOnResize();
 
 window.addEventListener('scroll', function(){
-    setScrollHeights();
-    var scroll_marker = 0; 
+    setScrollHeight();
+
     if(scroll_from_top >= projects_offset_top - window_height &&
 	scroll_from_top <= projects_offset_bottom){
+
 	var bkg_offset = scroll_from_top - projects_offset_top * -1;
 	document.getElementById('projects').style['background-position-y'] = bkg_offset + "px"; 
-}
+    }
     else{
 	document.getElementById('projects').style['background-position-y'] = "0px";
     }
 }); 
+
 window.addEventListener('resize', function(){
-    setScrollHeights();
-}); 
-function setScrollHeights(){
+    setLengthsOnResize();
+});
+
+function setScrollHeight(){
+	scroll_from_top = document.getElementsByTagName('html')[0].scrollTop;
+}
+function setLengthsOnResize(){
 	projects_offset_top = document.getElementById('projects').offsetTop; 
 	projects_div_height = document.getElementById('projects').clientHeight;
 	projects_offset_bottom = projects_offset_top + projects_div_height; 
-	scroll_from_top = document.getElementsByTagName('html')[0].scrollTop;
-	window_height = window.innerHeight; 
+	window_height = window.innerHeight;
+
 }
 function scrollToTop(){
 	document.getElementByTagName('html')[0].scroll(0,0); 
